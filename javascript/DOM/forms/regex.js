f85a36d6ac7688612a21a1ad74733fdb4217a25b
johann <johann.suarez92@gmail.com>
2021-02-13 00:13:49 -0800

    Regaining momentum — Reviewing regex on forms

--- javascript/DOM/forms/regex.js
+++ javascript/DOM/forms/regex.js
@@ -8,6 +8,10 @@
 /*
     Example: /^[a-zA-Z0-9]{6,10}$/gm
 
+    ^ AND $
+    means the input must follow the pattern from the beginning ( ^ ) 
+    to the end ( $ qwdq) 
+
     Any combination of uppercase and lowercase word with numbers
     that are 6 to 10 characters long.
 
@@ -21,39 +25,37 @@
 const form = document.querySelector('.signup-form');
 
 
+//-- HOW TO USE A REGEX IN JS --//
+
+/* 
+    A regular expression is always
+    between two forward slashes.
+
+*/
+
+
+/*
+    I just learned that any value that isn't 0
+    evaluates to TRUE in javascript (even if it's negative!!)
+*/
+
+
 form.addEventListener('submit', e => {
 
     e.preventDefault();
     // A nice quick way we can get fields from a form.
     // You can do this with id's and name attributes
-    console.log(form.username.value);
+
+    let input = form.username.value;
+    console.log(input);
+
+    let re = /^[a-z]{6,}$/;
+    result3 = re.test(input);
+
+    if (result3) { 
+        console.log('This entry passes regex check.');
+    } else {
+    
+        console.log('No, not this one.');
+    }
 });
-
-//-- HOW TO USE A REGEX IN JS --//
-
-const username = 'johann';
-const pattern = /[a-z]{6,}/;
-
-/* 
-    Regex data types have a method called 'test'
-    Feed it an input string, and it will return a boolean.
-*/
-let result = pattern.test(username);
-console.log(result);
-
-// Now we're making a little check
-
-if(result){
-    console.log('regex test passed ;)')
-} else console.log('regex token failed.');
-
-/* This is the opposite of pattern.test(username);
-   This returns an integer where it finds a pattern match
-   in the string.
-*/
-let result2 = username.search(pattern);
-console.log(result2);
-
-/* If it's negative, it didn't have a match.
-   We can use that as a boolean check.
-*/
